52e9d16bf4d83ae5d181acae49fa15e45b6c0b15
alves.valentin <valentin-alves@hotmail.com>
2025-05-01 04:26:10 +0200

    extract methods

--- src/katas/mars-rover/tests/mars-rover.ts
+++ src/katas/mars-rover/tests/mars-rover.ts
@@ -1,3 +1,5 @@
+const authorizedCommands = ['M', 'R', 'L'];
+
 export class MarsRover {
     constructor(private xCoordinate: number, private yCoordinate: number, private direction: string) {
 
@@ -6,81 +8,121 @@ export class MarsRover {
 
     execute(command: string) {
 
-        if (['M', 'R', 'L'].every(c => !command.includes(c))) {
-            throw new Error(`Unknown command: ${command}`)
+        this.validateCommand(command);
+
+        const instructions = this.splitCommandIntoInstructions(command);
+
+        this.processInstructions(instructions);
+    }
+
+    private processInstructions(instructions: string[]) {
+        instructions.forEach(instruction => {
+            this.moveNorth(instruction);
+
+            this.moveSouth(instruction);
+
+            this.moveEast(instruction);
+
+            this.moveWest(instruction);
+
+            this.rotateRight(instruction);
+
+            this.rotateLeft(instruction);
+
+            this.checkBounds();
+        })
+    }
+
+    private checkBounds() {
+        if (this.xCoordinate >= 5) {
+            this.xCoordinate = 0
         }
 
-        const commands = command.split('');
+        if (this.xCoordinate == -1) {
+            this.xCoordinate = 4
+        }
 
-        commands.forEach(command => {
-            if (command === 'M' && this.direction === 'N') {
-                this.yCoordinate++
+        if (this.yCoordinate >= 5) {
+            this.yCoordinate = 0
+        }
+
+        if (this.yCoordinate == -1) {
+            this.yCoordinate = 4
+        }
+    }
+
+    private rotateLeft(instruction: string) {
+        if (instruction === 'L') {
+            switch (this.direction) {
+                case 'N':
+                    this.direction = 'W'
+                    break;
+
+                case 'E':
+                    this.direction = 'N'
+                    break;
+                case 'S':
+                    this.direction = 'E'
+                    break;
+                case 'W':
+                    this.direction = 'S'
+                    break;
             }
+        }
+    }
 
-            if (command === 'M' && this.direction === 'S') {
-                this.yCoordinate--
+    private rotateRight(instruction: string) {
+        if (instruction === 'R') {
+            switch (this.direction) {
+                case 'N':
+                    this.direction = 'E'
+                    break;
+                case 'E':
+                    this.direction = 'S'
+                    break;
+                case 'S':
+                    this.direction = 'W'
+                    break;
+                case 'W':
+                    this.direction = 'N'
+
+
             }
+        }
+    }
 
-            if (command === 'M' && this.direction === 'E') {
-                this.xCoordinate++
-            }
+    private moveWest(instruction: string) {
+        if (instruction === 'M' && this.direction === 'W') {
+            this.xCoordinate--
+        }
+    }
 
-            if (command === 'M' && this.direction === 'W') {
-                this.xCoordinate--
-            }
+    private moveEast(instruction: string) {
+        if (instruction === 'M' && this.direction === 'E') {
+            this.xCoordinate++
+        }
+    }
 
-            if (command === 'R') {
-                switch (this.direction) {
-                    case 'N':
-                        this.direction = 'E'
-                        break;
-                    case 'E':
-                        this.direction = 'S'
-                        break;
-                    case 'S':
-                        this.direction = 'W'
-                        break;
-                    case 'W':
-                        this.direction = 'N'
+    private moveSouth(command: string) {
+        if (command === 'M' && this.direction === 'S') {
+            this.yCoordinate--
+        }
+    }
 
+    private moveNorth(command: string) {
+        if (command === 'M' && this.direction === 'N') {
+            this.yCoordinate++
+        }
+    }
 
-                }
-            }
+    private splitCommandIntoInstructions(command: string) {
+        return command.split('');
+    }
 
-            if (command === 'L') {
-                switch (this.direction) {
-                    case 'N':
-                        this.direction = 'W'
-                        break;
-
-                    case 'E':
-                        this.direction = 'N'
-                        break;
-                    case 'S':
-                        this.direction = 'E'
-                        break;
-                    case 'W':
-                        this.direction = 'S'
-                        break;
-                }
-            }
-
-            if (this.xCoordinate >= 5) {
-                this.xCoordinate = 0
-            }
-
-            if (this.xCoordinate  == -1) {
-                this.xCoordinate = 4
-            }
-
-            if (this.yCoordinate >= 5) {
-                this.yCoordinate = 0
-            }
-
-            if (this.yCoordinate  == -1) {
-                this.yCoordinate = 4
-            }
-        })
+    private validateCommand(command: string) {
+        if (authorizedCommands.every(c => !command.includes(c))) {
+            throw new Error(`Unknown command: ${command}`)
+        }
     }
 
     getPosition() {
